0b530c9ddcb707f9104485693e36d35438f6d998
Kyle Ryan <kryan14@u.rochester.edu>
2016-05-22 11:58:18 -0400

    Shutter CALayer subclass Implementation

---
+++ Caketalk/Shutter.swift
@@ -0,0 +1,107 @@
+//
+//  Shutter.swift
+//  Shutter
+//
+//  Created by Olivier Lesnicki on 20/06/2015.
+//  Copyright (c) 2015 LEMOTIF. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class Shutter {
+    
+    var layers : [ShutterLayer]
+    var path : String
+    
+    init(path: String, layers: [ShutterLayer]) {
+        self.layers = layers
+        self.path = path
+    }
+    
+    func export(exportPath: String, callback: () -> (Void)) {
+        
+        let videoUrl = NSURL(fileURLWithPath: path)
+        let video = AVURLAsset(URL: videoUrl, options: nil)
+        let videoTracks = video.tracksWithMediaType(AVMediaTypeVideo)
+        let videoTrack : AVAssetTrack = videoTracks[0] 
+        
+        let composition = AVMutableComposition()
+        let compositionTrackForVideo = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        
+        do {
+            try compositionTrackForVideo.insertTimeRange(
+                CMTimeRangeMake(kCMTimeZero, video.duration),
+                ofTrack: videoTrack ,
+                atTime: kCMTimeZero)
+        } catch _ {
+        }
+        
+        compositionTrackForVideo.preferredTransform = videoTrack.preferredTransform
+        
+        let size = compositionTrackForVideo.naturalSize
+                
+        let parentLayer = CALayer()
+        parentLayer.frame = CGRectMake(0, 0, size.width, size.height)
+        
+        let videoLayer = CALayer()
+        videoLayer.frame = CGRectMake(0, 0, size.width, size.height)
+        
+        let overlayLayer = CALayer()
+        overlayLayer.frame = CGRectMake(0, 0, size.width, size.height)
+        
+        for layer in layers {
+            layer.resize(size)
+            overlayLayer.addSublayer(layer)
+        }
+        
+        parentLayer.addSublayer(videoLayer)
+        parentLayer.addSublayer(overlayLayer)
+        
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.renderSize = size
+        videoComposition.frameDuration = CMTimeMake(1, 30)
+
+        videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(
+            postProcessingAsVideoLayer: videoLayer,
+            inLayer: parentLayer
+        )
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration);
+        
+        let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
+        instruction.layerInstructions = [layerInstruction]
+
+        videoComposition.instructions = [instruction]
+        
+        if (NSFileManager.defaultManager().fileExistsAtPath(exportPath)) {
+            do {
+                try NSFileManager.defaultManager().removeItemAtPath(exportPath)
+            } catch _ {
+            }
+        }
+        
+        let exportURL = NSURL(fileURLWithPath: exportPath)
+        
+        let export = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality)!
+        export.videoComposition = videoComposition
+        export.outputURL = exportURL
+        export.outputFileType = AVFileTypeQuickTimeMovie
+        export.shouldOptimizeForNetworkUse = true
+
+        export.exportAsynchronouslyWithCompletionHandler({
+            dispatch_async(dispatch_get_main_queue()) {
+                 print("done")
+                print(export.status, export.error)
+                callback()
+            }
+        })
+        
+    }
+    
+    
+    
+}
---
+++ Caketalk/ShutterLayer.swift
@@ -0,0 +1,61 @@
+//
+//  ShutterLayer.swift
+//  Shutter
+//
+//  Created by Olivier Lesnicki on 20/06/2015.
+//  Copyright (c) 2015 LEMOTIF. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+import AVFoundation
+
+class ShutterLayer : CALayer {
+    
+    init(title: String, yPosition : CGFloat, sequenceNumber : Double) {
+        
+        super.init()
+        
+        let overlayerLayer = CALayer()
+        overlayerLayer.frame = self.bounds
+        overlayerLayer.backgroundColor = UIColor.blackColor().CGColor
+        overlayerLayer.opacity = 0.2
+        overlayerLayer.masksToBounds = true
+        self.addSublayer(overlayerLayer)
+        
+        let textLayer = CATextLayer()
+        textLayer.font = UIFont.systemFontOfSize(26, weight: UIFontWeightSemibold)
+        textLayer.string = title
+        textLayer.foregroundColor = UIColor.whiteColor().CGColor
+        textLayer.frame = CGRectMake(30, yPosition, 350, 50)
+        textLayer.alignmentMode = kCAAlignmentLeft;
+        self.addSublayer(textLayer)
+        
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.duration = 1
+        animation.beginTime = sequenceNumber * 2
+        animation.fromValue = NSNumber(float: 0)
+        animation.toValue = NSNumber(float: 1)
+        animation.removedOnCompletion = false
+        animation.fillMode = kCAFillModeBoth
+        animation.additive = false
+        textLayer.addAnimation(animation, forKey: "opacityIN")
+
+//        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//        animation.beginTime = CMTimeGetSeconds(img.startTime);
+//        animation.duration = CMTimeGetSeconds(_timeline.transitionDuration);
+//        animation.fromValue = [NSNumber numberWithFloat:0.0f];
+//        animation.toValue = [NSNumber numberWithFloat:1.0f];
+//        animation.removedOnCompletion = NO;
+//        animation.fillMode = kCAFillModeBoth;
+//        animation.additive = NO;
+//        [parentLayer addAnimation:animation forKey:@"opacityIN"];
+
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
