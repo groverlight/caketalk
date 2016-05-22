1fa2d7300341864a674cb2f67bbef0f1845808a7
Kyle Ryan <kryan14@u.rochester.edu>
2016-05-22 11:52:17 -0400

    Splash.swift with video player background

--- Caketalk/splash.swift
+++ Caketalk/splash.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import Player
 
-class splash: UIViewController {
+class splash: UIViewController, PlayerDelegate {
+    
+    let videoUrl = NSURL(string: "https://v.cdn.vine.co/r/videos/AA3C120C521177175800441692160_38f2cbd1ffb.1.5.13763579289575020226.mp4")!
+    
+    private var player: Player!
 
 /*---------------BEGIN OUTLETS 🎛----------------------*/
 
@@ -21,17 +26,26 @@ class splash: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("login view loaded")
-
+        
+        self.player = Player()
+        self.player.delegate = self
+        self.player.view.frame = self.view.bounds
+        self.player.view.backgroundColor = .clearColor()
+        self.addChildViewController(self.player)
+        self.view.addSubview(self.player.view)
+        self.view.sendSubviewToBack(self.player.view)
+        self.player.didMoveToParentViewController(self)
+        
+        self.player.setUrl(videoUrl)
+        
+        self.player.playbackLoops = true
+        self.player.muted = true
+        self.player.playFromBeginning()
+        
 /*---------------BEGIN STYLE 🎨----------------------*/
-
-
-self.loginButton.layer.cornerRadius = 6
-
-
+        self.loginButton.layer.cornerRadius = 6
 /*---------------END STYLE 🎨----------------------*/
-
-
-}
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -39,12 +53,33 @@ self.loginButton.layer.cornerRadius = 6
     }
     
 
-//MARK: This function calls permission priming
+    //MARK: This function calls permission priming
 
     @IBAction func loginButtonPressed(sender: AnyObject) {
         print("login button pressed")
 
     }
 
+// MARK: PlayerDelegate
+    
+    func playerReady(player: Player) {
+    
+    }
+    
+    func playerPlaybackStateDidChange(player: Player) {
+    
+    }
+    
+    func playerBufferingStateDidChange(player: Player) {
+    
+    }
+    
+    func playerPlaybackWillStartFromBeginning(player: Player) {
+    
+    }
+    
+    func playerPlaybackDidEnd(player: Player) {
+    
+    }
 
 }
