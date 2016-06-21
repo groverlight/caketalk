19610cd5a56f22919a4cbf03ac1cb920c37df3a8
Kyle Ryan <kryan14@u.rochester.edu>
2016-06-21 19:54:57 -0400

    Stash

--- Pods/Target Support Files/Pods-Caketalk/Pods-Caketalk-frameworks.sh
+++ Pods/Target Support Files/Pods-Caketalk/Pods-Caketalk-frameworks.sh
@@ -84,8 +84,16 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Mixpanel/Mixpanel.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Player/Player.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/YUGPUImageHighPassSkinSmoothing/YUGPUImageHighPassSkinSmoothing.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Mixpanel/Mixpanel.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Player/Player.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/YUGPUImageHighPassSkinSmoothing/YUGPUImageHighPassSkinSmoothing.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
